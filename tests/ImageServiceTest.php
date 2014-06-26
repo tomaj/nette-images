e9acc17c46a3dee28ff7ed4737fc770c43a8d17d
Tomas Majer <tomasmajer@gmail.com>
2014-06-26 11:02:48 +0200

    Added simple always true test

---
+++ tests/ImageServiceTest.php
@@ -0,0 +1,11 @@
+<?php
+
+require dirname(__FILE__). '/../vendor/autoload.php';
+
+class ImageServiceTest extends PHPUnit_Framework_TestCase 
+{
+	public function testTrue()
+    {
+        $this->assertEquals(true, true);
+    }
+}
